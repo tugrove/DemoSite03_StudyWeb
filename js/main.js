caaa6e965d32458770850e14908ba1f3a2217d8b
tugrove <asmallpondinalargecastle@gmail.com>
2021-02-22 13:19:14 +0900

    [update]refs #5 main.jsの構成を整理
    今まで雑然とコードが並んでいたのを、変数定義、オブジェクト定義、実行処理に分割整理した

--- js/main.js
+++ js/main.js
@@ -1,32 +1,27 @@
-$( () => {
-    $(`#sideMenuButton`).on(`click`, () => {
-        operateSideMenu.toggle();
-    });
-    $(`#sideMenu a`).on(`click`, () => {
-        operateSideMenu.close();
-    });
-    $(`#sideMenu .globalNav .parent-ul .parent-li span`).on(`click`, event => {
-        const parent = event.currentTarget.parentElement;
-        operateSideMenu.globalNav.toggleChildUl(parent);
-    });
-});
+/* 変数定義 ************************************/
 
+// 定数
+const OPEN = `open`;
+
+/* オブジェクト定義 *****************************/
+
+// サイドメニューの開閉
 const operateSideMenu = {
 
     open: function() {
-        $(`#sideMenuButton`).addClass(`open`);
-        $(`#sideMenu`).addClass(`open`);
+        $(`#sideMenuButton`).addClass(OPEN);
+        $(`#sideMenu`).addClass(OPEN);
         this.globalNav.closeChildUl();
     },
 
     close: function() {
-        $(`#sideMenuButton`).removeClass(`open`);
-        $(`#sideMenu`).removeClass(`open`);
+        $(`#sideMenuButton`).removeClass(OPEN);
+        $(`#sideMenu`).removeClass(OPEN);
     },
 
     toggle: function() {
         const getClass = $(`#sideMenuButton`).attr(`class`);
-        if (getClass === `open`) {
+        if (getClass === OPEN) {
             this.close();
         } else {
             this.open();
@@ -36,13 +31,30 @@ const operateSideMenu = {
     globalNav: {
 
         toggleChildUl: function(elem) {
-            elem.classList.toggle(`open`);
+            elem.classList.toggle(OPEN);
         },
 
         closeChildUl: function() {
-            $(`#sideMenu .globalNav .parent-ul .parent-li`).removeClass(`open`);
+            $(`#sideMenu .globalNav .parent-ul .parent-li`).removeClass(OPEN);
         }
-
     }
-
 };
+
+/* HTMLの読み込み後の処理 ***********************/
+$( () => {
+    // サイドメニューボタンを押した際の処理
+    $(`#sideMenuButton`).on(`click`, () => {
+        operateSideMenu.toggle();
+    });
+    // サイドメニュー内のリンクを踏んだ際にサイドメニューを閉じる処理
+    $(`#sideMenu a`).on(`click`, () => {
+        operateSideMenu.close();
+    });
+    // サイドメニュー内のアコーディオンメニューを開閉する処理
+    $(`#sideMenu .globalNav .parent-ul .parent-li span`).on(`click`, event => {
+        const parent = event.currentTarget.parentElement;
+        operateSideMenu.globalNav.toggleChildUl(parent);
+    });
+});
+
+
