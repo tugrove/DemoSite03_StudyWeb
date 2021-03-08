e077e9ef6324a9b310c971c1b11eaf206b704716
tugrove <asmallpondinalargecastle@gmail.com>
2021-03-08 12:58:54 +0900

    [add]refs #65 galleryページに動画を選択できる機能を追加

--- js/apiYouTube.js
+++ js/apiYouTube.js
@@ -2,16 +2,15 @@
 /* ============================================ */
 
 // APIのためのパラメタ
-const type = `video`;                                  // 動画検索を指定
-const part = `snippet`;                                // 検索結果に全てのプロパティを含む
-let q = `cake`;                                        // 初期検索ワードを`cake`に設定
-const videoEmbeddable = `true`;                        // Webページに埋め込み可能な動画を検索
-const videoSyndicated = `true`;                        // youtube.com以外で再生可能な動画のみ取得
-const maxResults = 3;                                  // 動画の最大取得数
+const type = `video`;           // 動画検索を指定
+const part = `snippet`;         // 検索結果に全てのプロパティを含む
+const videoEmbeddable = `true`; // Webページに埋め込み可能な動画を検索
+const videoSyndicated = `true`; // youtube.com以外で再生可能な動画のみ取得
+const maxResults = 3;           // 動画の最大取得数
 const key = `AIzaSyCuRRugd7JW05COyePp9e5_F5euXrIz4zs`; // APIキー
 
 // 動画を表示するiframeの親要素divにつけるタグの名前
-const IFRAMEWRAP = `video`;
+const IFRAMEWRAP = `YouTube`;
 
 /* 関数定義 */
 /* ============================================ */
@@ -28,39 +27,63 @@ const urijoin = (strings, ...values) => {
     return result.join("");
 };
 
-// データ取得が成功した際に、動画を表示する関数
-// div#YouTube内に取得した動画件数文のiframeを作成する
-const setYouTube = (data) => {
-    const result = [];
-    // 動画を表示するHTMLを作る
-    for (let item of data.items) {
-        const video = `<iframe src="https://www.youtube.com/embed/${item.id.videoId}" allowfullscreen></iframe>`;
-        result.push(`<div class="${IFRAMEWRAP}">${video}</div>`);
+
+/* オブジェクト定義 */
+/* ============================================ */
+
+const operateMovie = {
+
+    // データ取得が成功した際に、動画を表示するメソッド
+    // #YouTubeMovies内に取得した動画件数分のiframe要素を作成する
+    setMovies: function(data) {
+        // 取得した動画を表示するiframe要素を作成
+        const result = [];
+        for (let item of data.items) {
+            const iframe = urijoin`<iframe src="https://www.youtube.com/embed/${item.id.videoId}" allowfullscreen></iframe>`;
+            result.push(`<div class="${IFRAMEWRAP}">${iframe}</div>`);
+        }
+        result.join("");
+        // #YouTubeMoviesの中身を変更する
+        $(`.page-gallery #pageMain #galleryMovie #YouTubeMovies`).html(result);
+    },
+
+    // YouTubeAPIによって動画情報を取得するメソッド
+    // API用のURLを引数として渡す
+    ajaxYouTube: function(url) {
+        // youtubeの動画を検索して取得
+        $.ajax({
+            url: url,
+            dataType: `jsonp`
+        }).done( data => {
+            if (data.items) {
+                this.setMovies(data);
+            } else {
+                console.log(data);
+                alert('該当するデータが見つかりませんでした');
+            }
+        }).fail( data => {
+            alert(`通信に失敗しました`);
+        });
+    },
+
+    // movieCategoryのからYouTubeAPIの検索キーワードを取得し、URLを作成して、ajaxを行うメソッド
+    selectMovie: function() {
+        const q = $(`.page-gallery #pageMain #galleryMovie #movieCategory`).val();
+        const url = urijoin`https://www.googleapis.com/youtube/v3/search?type=${type}&part=${part}&q=${q}&videoEmbeddable=${videoEmbeddable}&videoSyndicated=${videoSyndicated}&maxResults=${maxResults}&key=${key}`;
+        this.ajaxYouTube(url);
     }
-    result.join("");
-    // HTMLに反映する
-    $(`.page-gallery #pageMain #galleryMovie #YouTubeVideos`).html(result);
 };
 
 
 /* HTMLの読み込み後の処理 */
 /* ============================================ */
-let url = urijoin`https://www.googleapis.com/youtube/v3/search?type=${type}&part=${part}&q=${q}&videoEmbeddable=${videoEmbeddable}&videoSyndicated=${videoSyndicated}&maxResults=${maxResults}&key=${key}`;
 
 $( () => {
-    // youtubeの動画を検索して取得
-    $.ajax({
-        url: url,
-        dataType: `jsonp`
-    }).done( data => {
-        if (data.items) {
-            setYouTube(data);
-        } else {
-            console.log(data);
-            alert('該当するデータが見つかりませんでした');
-        }
-    }).fail( data => {
-        alert(`通信に失敗しました`);
+    // はじめにYouTubeAPIを取得する
+    operateMovie.selectMovie();
+    // #movieCategoryが変更されたらYouTubeAPIを取得しなおす
+    $(`.page-gallery #pageMain #galleryMovie #movieCategory`).on(`change`, () => {
+        operateMovie.selectMovie();
     });
 });
 
